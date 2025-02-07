885dcbb0998ea85d79c88d59637ab64df7e68d4d
Amaljith <amaljithkuniyil.com>
2025-02-08 05:13:43 +0530

    Fixed csrf issue

--- MedCare/settings.py
+++ MedCare/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-leh0#ai=8raw5r55pvja#5g85t-!wx(pj0(!wsyq(1w&0igdr0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*",
                  "web-production-82524.up.railway.app",
