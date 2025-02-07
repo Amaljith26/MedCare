97d044935eebcd1773a594d69352d3e425d0c8fe
Amaljith <amaljithkuniyil.com>
2025-02-08 01:14:48 +0530

    Fixed static file paths and updated runtime.txt

--- MedCare/urls.py
+++ MedCare/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('',include('signup.urls')),
     path('', include('doctors.urls'))
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
